86e88b8e6191a6680b5e52b11e7d86dfa2a7e333
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 15:51:32 -0300

    lista 5, atividade 3

---
+++ Lista 05/incluir.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Sou o arquivo incluir.";
+echo "<br>";
+include_once("boletos.php");
+echo "<br>";
+include_once("boletos.php");
+echo "<br>";
+include_once("boletos.php");
+echo "<br>";
+
+?>
