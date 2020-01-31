1dc41b2b2386b702b9f7120d14a626613655a1e5
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 14:49:35 -0300

    atividade 1

---
+++ Lista 01/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// exercicio 01
+// duvida no item g e h
+    $num = 2;
+    echo $num;
+    echo"<br>";
+    var_dump ($num);
+    echo"<br>";
+
+    $dec = 2.5;
+    echo $dec;
+    echo"<br>";
+    var_dump ($dec);
+    echo"<br>";
+
+    $simples = 'E';
+    echo $simples;
+    echo"<br>";
+    var_dump ($simples);
+    echo"<br>";
+
+    $dupla = "L";
+    echo $dupla;
+    echo"<br>";
+    var_dump ($dupla);
+    echo"<br>";
+
+    $simplesdois = 'Elke';
+    echo $simplesdois;
+    echo"<br>";
+    var_dump ($simplesdois);
+    echo"<br>";
+
+    $dupladois = "Lorena";
+    echo $dupladois;
+    echo"<br>";
+    var_dump ($dupladois);
+    echo"<br>";
+
+    $num = "string";
+    echo $num;
+    echo"<br>";
+    var_dump($num);
+    echo"<br>";
+
+    $simplesdois = 2.6;
+    echo $simplesdois;
+    echo"<br>";
+    var_dump($simplesdois);
+    echo"<br>";
+
+    $num ="3.5";
+    echo $num;
+    echo"<br>";
+    var_dump($num);
+    echo"<br>";
+
+    
+</body>
+</html>
