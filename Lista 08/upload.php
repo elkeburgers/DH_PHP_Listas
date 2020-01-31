d8ef8e86c98ec51719ee93b925d477b2c7b20546
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 20:46:01 -0300

    lista 8, atividade 2

--- Lista 08/upload.php
+++ Lista 08/upload.php
@@ -12,3 +12,31 @@
         <input type="submit" value="Carregar foto" />
     </form>
     <br>.<br>
+
+    <!-- exercício 2 -->
+
+    <form>
+    <p> Formulário de cadastro</p>
+        Nome:<br>
+        <input type="text" name="nome"><br>
+        <br>
+        Telefone:<br>
+        <input type="number" name="telefone"><br>
+        <br>
+        <form method="post" action="perfil.php" enctype="multipart/form-data">
+            <label>Arquivo:</label>
+            <input type="file" name="imgPerfil" />
+            <br>.<br>
+            <input type="submit" value="Carregar foto" />
+        </form>
+        <br>.<br>
+
+
+        <?php
+
+
+?>
+
+</body>
+
+</html>
