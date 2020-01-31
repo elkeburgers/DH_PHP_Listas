a6bb63a974a055d1cf6289b37239836d05848eb9
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 15:26:17 -0300

    lista 4, atividade 3

--- Lista 04/imprimir.php
+++ Lista 04/imprimir.php
@@ -35,4 +35,27 @@ var_dump($_GET);
 echo "<br>"."<br>";
 ?>
 
+<!-- exercício 03 -->
+
+<!DOCTYPE html>
+
+<body>
+    <form action="atividade4.php" method="post">
+        Nome: <input type="text" name="nome"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
+<?php
+var_dump($_POST);
+echo "<br>"."<br>";
+
+foreach($_POST as $dados=> $valor){
+    echo "$dados : $valor"."<br>";
+}
+var_dump($_POST);
+echo "<br>"."<br>";
+?>
 
