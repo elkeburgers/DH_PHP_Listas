bdefbe2191a8dbe4fed67164c65fc66fe5a1f82c
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 14:58:50 -0300

    lista 2, atividade 1

---
+++ Lista 02/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// exercício 01
+$vara = 5;
+$varb = 12;
+if ($vara > $varb){
+    echo "O maior número é ".$vara;
+}else{
+    echo " O maior número é ".$varb;
+}
+echo"<br>";
+
+
+?>
+</body>
+</html>
