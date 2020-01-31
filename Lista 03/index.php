94ebd64759224f110b3b8013d078db6d8a3f2744
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 15:11:59 -0300

    lista 3, atividade 2

--- Lista 03/index.php
+++ Lista 03/index.php
@@ -7,6 +7,14 @@ for ($i = 0; $i <= 100; $i++){
 }
 echo"<br>"."<br>";
 
+// exercício 02
+$pare = mt_rand(0,100);
+echo $pare;
+echo "<br>";
+for ($i = 0; $i <= $pare; $i++){
+    echo $i." ";
+}
+echo"<br>"."<br>";
 
 
 ?>
