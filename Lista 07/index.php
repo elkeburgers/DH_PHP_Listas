c0eb0d666ca485e4b8f3f383d0929ee540ae8a9c
Elke Burgers <elke.burgers@hotmail.com>
2020-01-31 20:25:26 -0300

    lista 7 - atividade 2

--- Lista 07/index.php
+++ Lista 07/index.php
@@ -22,6 +22,24 @@ echo "<br>"."<br>";
 echo $b["c"]." | ".$b["a"]." | ".$b["b"];
 echo "<br>"."<br>";
 
+// exercício 02
+
+$categorias = json_decode(file_get_contents("categorias.json"),true);
+echo "<pre>";
+var_dump($categorias);
+echo "</pre>";
+echo "<br>"."<br>";
+
+
+foreach($categorias["categorias"] as $chave=>$valor){
+  // var_dump ($valor);
+  // echo "<br>"."<br>";
+  echo "<input id=$valor[nome] type='checkbox'>";
+  echo "<label for=$valor[nome]>$valor[nome]</label>";
+  // echo ;
+  echo "<br>"."<br>";
+}
+
 
 
 
